210de89a3cbc196cc0babdaa5c82f52220254259
Cahit Atilla Bozkurt <cbozkurt@k1m23s03.42kocaeli.com.tr>
2026-08-30 13:52:49 +0300

    camera look done

--- main.py
+++ main.py
@@ -5,27 +5,37 @@ class Player:
     def __init__(self, position):
         pass
 
+camera: rl.Camera3D = rl.Camera3D()
+camera.position = rl.Vector3(1, 0.5, 0)
+camera.target = rl.Vector3(1, 0.5, 1)
+camera.up = rl.Vector3(0, 1, 0)
+camera.fovy = 60.0
+camera.projection = rl.CameraProjection.CAMERA_PERSPECTIVE
+camera_mode = 1
+
+def Input():
+    mouse_delta = rl.get_mouse_delta()
+    rotation = rl.Vector3(
+            mouse_delta.x * 0.05,
+            mouse_delta.y * 0.05,
+            0.0
+            )
+    rl.update_camera_pro(camera, (0, 0, 0), rotation, 0.0)
+
+
 def main():
     rl.init_window(1024, 768, "Raylib")
     rl.rl_set_line_width(3)
     rl.set_target_fps(60)
-    angle: float = 0.0
+    rl.disable_cursor()
 
-    camera: rl.Camera3D = rl.Camera3D((0, 0.5, 0),
-                                      (1, 0.5, 1),
-                                      (0, 1, 0),
-                                      60)
     map_size = 10
 
     while not rl.window_should_close():
-        angle += 0.001
-        camera.position.x = math.cos(angle) * 5
-        camera.position.z = math.sin(angle) * 5
-
         rl.begin_drawing()
         rl.clear_background(rl.SKYBLUE)
-        rl.draw_text(str(rl.get_mouse_x()), 10, 10, 100, rl.WHITE)
         rl.begin_mode_3d(camera)
+        Input()
         rl.draw_grid(map_size * 10, 0.2)
         rl.draw_plane((0, 0.5, 0), (map_size * 2, map_size * 2), rl.WHITE)
         rl.end_mode_3d()
